f32d8a2f91a30e1ccbddb3cc675d122d24a6bc17
Chris Neale <cin210@nyu.edu>
2012-04-23 14:41:24 -0400

    Initial Commit

---
+++ thirdeye/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  thirdeye
+//
+//  Created by Kathryn Saxton on 4/23/12.
+//  Copyright (c) 2012 Yale University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
