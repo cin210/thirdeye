9eaf1341c5db676d045b058dc184f3de118a6e69
cin210@nyu.edu <cin210@nyu.edu>
2012-05-11 13:42:47 -0400

    pruning

--- thirdeye/ListOfLocationsController.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ListOfLocationsController.cpp
-//  thirdeye
-//
-//  Created by Kathryn Saxton on 5/5/12.
-//  Copyright (c) 2012 Yale University. All rights reserved.
-//
-
-#include <iostream>
